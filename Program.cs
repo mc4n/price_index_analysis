e08fa0a9a79bd60d6cd8ebd56cdc290b1e177647
Mustaph John <mustcean@gmail.com>
2022-06-24 15:15:34 +0300

    initial version

--- Program.cs
+++ Program.cs
@@ -1,70 +1,74 @@
 ﻿#region xu100_dict
 var xu100_hist = new Dictionary<int, int>()
 {
-    {87,-3},
-    {88,42},
-    {89,251},
+    {1987,-3},
+    {1988,42},
+    {1989,251},
 
-    {90,-8},
-    {91,-22},
-    {92,-45},
-    {93,202},
-    {94,-42},
-    {95,-18},
-    {96,36},
-    {97,78},
-    {98,-56},
-    {99,247},
+    {1990,-8},
+    {1991,-22},
+    {1992,-45},
+    {1993,202},
+    {1994,-42},
+    {1995,-18},
+    {1996,36},
+    {1997,78},
+    {1998,-56},
+    {1999,247},
 
-    {0,-55},
-    {1,-13},
-    {2,-42},
-    {3,59},
-    {4,23},
-    {5,48},
-    {6,-10},
-    {7,31},
-    {8,-56},
-    {9,85},
+    {2000,-55},
+    {2001,-13},
+    {2002,-42},
+    {2003,59},
+    {2004,23},
+    {2005,48},
+    {2006,-10},
+    {2007,31},
+    {2008,-56},
+    {2009,85},
 
-    {10,17},
-    {11,-30},
-    {12,44},
-    {13,-19},
-    {14,17},
-    {15,-23},
-    {16,0},
-    {17,32},
-    {18,-35},
-    {19,13},
+    {2010,17},
+    {2011,-30},
+    {2012,44},
+    {2013,-19},
+    {2014,17},
+    {2015,-23},
+    {2016,0},
+    {2017,32},
+    {2018,-35},
+    {2019,13},
 
-    {20,14},
-    {21,-12},
-    {22, -33}
+    {2020,14},
+    {2021,-12},
+    {2022, -33}
 };
 #endregion
 
 
-Console.WriteLine($"total change rate (in {xu100_hist.Count} years): %{xu100_hist.Values.Sum()}");
+Console.WriteLine($"total change rate (in {xu100_hist.Count} years): {xu100_hist.Values.Sum()}%");
 Console.WriteLine();
 
 const int GROUP_LEN = 4;
 
 const int START_INDEX = 0;
 
-for (int i = START_INDEX; i < xu100_hist.Count; i += GROUP_LEN)
 {
-    var g_pairs = xu100_hist.Skip(i).Take(GROUP_LEN).ToDictionary(x => x.Key, x => x.Value); ;
-
-    System.Console.WriteLine("group-" + (i / GROUP_LEN + 1));
-
-    var sub_i = 0;
-    foreach (var kvp in g_pairs)
+    for (int i = START_INDEX; i < xu100_hist.Count; i += GROUP_LEN)
     {
-        sub_i++;
-        Console.WriteLine("{0}.year: {1} => change: {2}", sub_i, kvp.Key, kvp.Value);
-    }
-    System.Console.WriteLine($"sum: {g_pairs.Values.Sum()}");
-    System.Console.WriteLine();
+        System.Console.WriteLine("==========================================");
+        var g_pairs = xu100_hist.Skip(i).Take(GROUP_LEN).ToDictionary(x => x.Key, x => x.Value);
 
+        var g_total = g_pairs.Values.Sum();
+
+        System.Console.WriteLine("group-" + (i / GROUP_LEN + 1).ToString("00") + ":");
+
+        var sub_i = 0;
+        foreach (var kvp in g_pairs)
+        {
+            sub_i++;
+            Console.WriteLine("{0}.year: {1} => change_rate: {2}%", sub_i, kvp.Key, kvp.Value);
+        }
+
+        System.Console.WriteLine($"sum: {g_total}");
+    }
 }
